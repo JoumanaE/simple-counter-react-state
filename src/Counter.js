61607103b07e75cff69490b64b93935a17054477
Joumana El Alaoui <joumana.codes@gmail.com>
2019-12-11 09:40:13 -0600

    adding the hook

--- src/Counter.js
+++ src/Counter.js
@@ -1,66 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const getStateFromLocalStorage = () => {
-  const storage = localStorage.getItem('counterState');
-  console.log(storage);
-  if (storage) return JSON.parse(storage);
-  return { count: 0 };
+// const getStateFromLocalStorage = () => {
+//   const storage = localStorage.getItem('counterState');
+//   if (storage) return JSON.parse(storage);
+//   return { count: 0 };
+// }
+// function storeStateInLocalStorage() {
+//   localStorage.setItem('counterStorage', JSON.stringify(this.state))
+//   console.log(localStorage);
+// }
+
+
+const Counter = ({ max, step }) => {
+  const [count, setCount] = React.useState(0);
+
+  const increment = () => {
+    setCount(c => {
+      if (c >= max) return c;
+      return c + step;
+    });
+  };
+
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  useEffect(() => {
+    document.title = `Counter: ${count}`;
+
+  }, []);
+
+  return (
+    <div className="Counter">
+      <p className="count">{count}</p>
+      <section className="controls">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </section>
+    </div>
+  );
+
 };
 
-const storeStateInLocalStorage = state => {
-  localStorage.setItem('counterState', JSON.stringify(state));
-  console.log(localStorage);
-};
 
-document.title = 'Hello';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = getStateFromLocalStorage();
-
-    console.log(this.state);
-
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-    this.reset = this.reset.bind(this);
-  }
-
-  increment() {
-    this.setState(
-      (state, props) => {
-        const { max, step } = props;
-        if (state.count >= max) return;
-        return { count: state.count + step };
-      },
-      () => storeStateInLocalStorage(this.state),
-    );
-
-    console.log('Before!', this.state);
-  }
-
-  decrement() {
-    this.setState({ count: this.state.count - 1 });
-  }
-
-  reset() {
-    this.setState({ count: 0 });
-  }
-
-  render() {
-    const { count } = this.state;
-
-    return (
-      <div className="Counter">
-        <p className="count">{count}</p>
-        <section className="controls">
-          <button onClick={this.increment}>Increment</button>
-          <button onClick={this.decrement}>Decrement</button>
-          <button onClick={this.reset}>Reset</button>
-        </section>
-      </div>
-    );
-  }
-}
-
-export default Counter;
+export default Counter; 
